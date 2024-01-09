35b8cc2e342101ee664f33bae5e19125e5cc3336
Vinith1710 <vinith13579@gmail.com>
2024-01-09 16:25:59 +0530

    Slider Added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-// var carouselWidth = $('.carousel-inner')[0].scrollWidth;
-// var cardWidth = $('.carousel-item').width();
-// var scrollPosition = 0;
+var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+var cardWidth = $('.carousel-item').width();
+var scrollPosition = 0;
 
-// $('carousel-control-next').on('click',function () {
-//     console.log('next');
-//     scrollPosition = scrollPosition + cardWidth;
-//     $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
-// });
+$('.carousel-control-next').on('click',function(){
+    if(scrollPosition < (carouselWidth - (cardWidth * 4))){
+        console.log('next');
+        scrollPosition = scrollPosition + cardWidth;
+        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
+    }
+});
+$('.carousel-control-prev').on('click',function(){
+    if(scrollPosition > 0 ){
+        console.log('prev');
+        scrollPosition = scrollPosition - cardWidth;
+        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
+    }
+});
